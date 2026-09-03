9ae68c155b47c98fa962f6c615c9a7c881f2d10f
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-03 16:40:21 +0000

    lab/lab01/ch01_t10_two_types_of_division.py

--- lab/lab01/ch01_t10_two_types_of_division.py
+++ lab/lab01/ch01_t10_two_types_of_division.py
@@ -4,4 +4,5 @@ num_people = 6
 whole_cucumbers_per_person = cucumbers // num_people
 print(whole_cucumbers_per_person)
 
-float_cucumbers
+float_cucumbers_per_person = cucumbers / num_people
+print()
