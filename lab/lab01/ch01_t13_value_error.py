6477a83bdd365cd19da8f67cd599c046fa883110
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-03 16:52:14 +0000

    [skip actions] lab/lab01/ch01_t13_value_error.py

--- lab/lab01/ch01_t13_value_error.py
+++ lab/lab01/ch01_t13_value_error.py
@@ -1,5 +1,5 @@
 float_1 = 0.25
 float_2 = 40.0
 
-product = float_1 * float_2
-big_string = "The product was " + str(product)
+produc = float_1 * float_2
+big_string = "The product was " + str(produc)
