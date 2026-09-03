48d8adc3dbd049423b2cdaede4a91beaf03ffae1
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-03 17:01:30 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,5 +1,7 @@
 # Assign your variables below, each on its own line!
-Set caesar to 
+Set caesar to "Graham"
+set praline to "John"
+Set viking to "Teresa"
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
 # print(caesar)
