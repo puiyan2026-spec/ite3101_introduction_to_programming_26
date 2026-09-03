3f66e74e82a03ad56981773549f815561bde41ee
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-03 17:41:39 +0000

    [skip actions] lab/lab02/ch02_t03_escaping_characters.py

--- lab/lab02/ch02_t03_escaping_characters.py
+++ lab/lab02/ch02_t03_escaping_characters.py
@@ -1,3 +1,3 @@
- print("The string below is broken. Fix it using the escape backslash!")
- print("Uncomment the following line!")
+print("The string below is broken. Fix it using the escape backslash!")
+print("Uncomment the following line!")
 print('This isn\'t flying, this is falling with style!')
