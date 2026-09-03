7a952aae6f1efa99874e8359f6e6e53147ff0cbf
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-03 17:03:37 +0000

    lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,7 +1,7 @@
 # Assign your variables below, each on its own line!
 caesar = "Graham"
 praline = "John"
- viking = "Teresa"
+viking = "Teresa"
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
 # print(caesar)
