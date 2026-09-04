bf1bef6ffedcf3b8dac906fc11ba9b635f330ce0
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-04 04:21:31 +0000

    [skip actions] lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -3,6 +3,7 @@ pyg = 'ay'
 original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
-    print(original)
+    word = original.lower();
+    rint(original)
 else:
     print('empty')
