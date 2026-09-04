8fe390f6123542e2e8e8064398c06e2ae2253e33
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-04 04:30:07 +0000

    lab/lab05/ch05_t10_mytest.py

--- lab/lab05/ch05_t09_ending_up.py
+++ lab/lab05/ch05_t09_ending_up.py
@@ -6,6 +6,7 @@ if len(original) > 0 and original.isalpha():
     word = original.lower()
     first = word[0]
     new_word = word + first + pyg
-    new_word = new_word[1:len(new_word)-1]
+    new_word = new_word[1:len(new_word)]
+    print(new_word)
 else:
     print('empty')
