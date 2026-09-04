0be53685d7b73335bbfe07cf94c18a08710bac29
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-04 02:36:23 +0000

    [skip actions] lab/lab02/ch02_t05_string_methods.py

--- lab/lab02/ch02_t05_string_methods.py
+++ lab/lab02/ch02_t05_string_methods.py
@@ -1,3 +1,2 @@
-# The string below is broken. Fix it using the escape backslash!
+# The string below is broken. Fix it using the escape baclslash!
 # Uncomment the following line!
-"This isn\'t flying, this is falling with style!"
