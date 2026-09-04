c67c9e93507804cae4ab8d34568330ae61839237
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-04 04:22:31 +0000

    [skip actions] lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -4,6 +4,7 @@ original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
     word = original.lower();
-    first = word[0]rint(original)
+    first = word[0]
+    new_word = word + first + rint(original)
 else:
     print('empty')
