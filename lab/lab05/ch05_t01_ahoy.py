9352c786492452f8f03b59acf3e3411a40a287fe
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-04 04:15:26 +0000

    lab/lab05/ch05_t01_ahoy.py

--- lab/lab05/ch05_t01_ahoy.py
+++ lab/lab05/ch05_t01_ahoy.py
@@ -1,9 +1,1 @@
-print('welcome to the Pig Latin Translator!')
-
-# Start coding here!
-orignial = input('Enter a word':)
-
-if len(original) > 0:
-    print(orignial)
-else:
-    print ("empty")
+print("Pig Latin")
