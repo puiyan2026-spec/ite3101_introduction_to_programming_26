e4132974f0fdc26c2780c3f5c1cddddf0c517e40
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-04 03:46:31 +0000

    [skip actions] lab/lab04/ch04_t03_compare_closelier.py

--- lab/lab04/ch04_t03_compare_closelier.py
+++ lab/lab04/ch04_t03_compare_closelier.py
@@ -8,10 +8,10 @@ bool_one = False  # We did this one for you!
 bool_two = False
 
 # 1**2 <= -1
-bool_three = None
+bool_three = False
 
 # 40 * 4 >= -4
-bool_four = None
+bool_four = False
 
 # 100 != 10**2
 bool_five = None
