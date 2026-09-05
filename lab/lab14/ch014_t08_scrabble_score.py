a701719037e7476b64118b72a9909038ecb9a888
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 03:25:06 +0000

    lab/lab14/ch014_t08_scrabble_score.py

--- lab/lab14/ch014_t08_scrabble_score.py
+++ lab/lab14/ch014_t08_scrabble_score.py
@@ -7,5 +7,5 @@ score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
 def scrabble_score(word: str) -> int:
     mark = 0
     for char in word.lower():
-    mark += score[char]
+         mark += score[char]
     return mark
