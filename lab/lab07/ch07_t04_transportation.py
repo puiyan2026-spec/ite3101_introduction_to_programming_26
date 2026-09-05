1dfa42137c8c38829b7ae61489ecfd4ac3a1c99f
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 04:47:51 +0000

    lab/lab07/ch07_t04_transportation.py

--- lab/lab07/ch07_t04_transportation.py
+++ lab/lab07/ch07_t04_transportation.py
@@ -12,7 +12,8 @@ def plane_ride_cost(city: str) -> int:
     elif city == "Los Angeles":
         return 475
 
-    def rental_car_cost(days: int) -> int:
+
+def rental_car_cost(days: int) -> int:
         cost = days * 40
         if days >= 7:
             cost -= 50
