3d6c5be9d2f2d334e509303ddfd2c973a9d81b54
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 03:13:19 +0000

    [skip actions] lab/lab14/ch014_t06_reverse.py

--- lab/lab14/ch014_t06_reverse.py
+++ lab/lab14/ch014_t06_reverse.py
@@ -1,7 +1,7 @@
-def reverse(text:str)->str:
-    word=""
-    length=len(text)-1
-    while length>0:
-        word+=+text[length]
-        length-=1
+def reverse(text: str) -> str:
+    word = ""
+    length = len(text)-1
+    while length > 0:
+        word += +text[length]
+        length -= 1
     return word
