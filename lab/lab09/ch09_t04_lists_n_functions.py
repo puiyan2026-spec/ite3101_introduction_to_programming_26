88546f45be143c7a58fb4db4dabea53e69a79d38
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 06:09:04 +0000

    [skip actions] lab/lab09/ch09_t04_lists_n_functions.py

--- lab/lab09/ch09_t04_lists_n_functions.py
+++ lab/lab09/ch09_t04_lists_n_functions.py
@@ -1,10 +1,11 @@
 # Write your function below!
 
-def fizz_count(x:list)
+def fizz_count(x: list)
 
 
-def fizz_count(x:list)->int:
-    count=0
+def fizz_count(x: list) -> int:
+    count = 0
     for word in words:
         if word == "fizz":
             count += 1
+    return count
