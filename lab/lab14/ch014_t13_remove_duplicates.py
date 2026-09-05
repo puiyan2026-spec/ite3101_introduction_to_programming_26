538a6f328c4465bba39f071f58bc0290f4384621
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 03:36:56 +0000

    lab/lab14/ch014_t13_remove_duplicates.py

--- lab/lab14/ch014_t13_remove_duplicates.py
+++ lab/lab14/ch014_t13_remove_duplicates.py
@@ -1,8 +1,9 @@
 from typing import List
 
 
-def remove_duplicates(seq: List[int])->List[int]:
-    result=[]
+def remove_duplicates(seq: List[int]) -> List[int]:
+    result = []
     for item in seq:
         if item not in result:
-            result
+            result.append(item)
+    return result
