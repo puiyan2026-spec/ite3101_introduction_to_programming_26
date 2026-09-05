82ea7009ba6c82279dcd55f71e33814e6e3cd18e
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 03:11:37 +0000

    lab/lab14/ch014_t05_is_prime.py

--- lab/lab14/ch014_t05_is_prime.py
+++ lab/lab14/ch014_t05_is_prime.py
@@ -2,7 +2,7 @@ def is_prime(x: int) -> bool:
     if x < 2:
         return False
     else:
-        for n in range(2, x-1)
+        for n in range(2, x-1):
             if x % n == 0:
                 return False
         return True
