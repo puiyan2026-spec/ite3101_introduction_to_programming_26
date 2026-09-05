3d107db8c61c94e415559dc78adc8e3f3cb1b579
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 03:36:35 +0000

    [skip actions] lab/lab14/ch014_t13_remove_duplicates.py

--- lab/lab14/ch014_t13_remove_duplicates.py
+++ lab/lab14/ch014_t13_remove_duplicates.py
@@ -1,4 +1,8 @@
 from typing import List
 
 
-def remove_duplicates(seq: List[])
+def remove_duplicates(seq: List[int])->List[int]:
+    result=[]
+    for item in seq:
+        if item not in result:
+            result
