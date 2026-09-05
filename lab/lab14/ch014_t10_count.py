63bf8c762b975115fdefb21d91c8e9e66c979ca1
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 03:29:21 +0000

    [skip actions] lab/lab14/ch014_t10_count.py

--- lab/lab14/ch014_t10_count.py
+++ lab/lab14/ch014_t10_count.py
@@ -1,1 +1,6 @@
-def count(sequence: List[int], item:int)-
+def count(sequence: List[int], item:int)->int:
+    result = 0
+    for i in sequence:
+        if i == item:
+            result += 1
+    return result 
