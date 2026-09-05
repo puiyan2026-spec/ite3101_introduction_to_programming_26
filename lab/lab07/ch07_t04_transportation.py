440497f698c6028472792742a76baa742fc3630e
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 04:46:42 +0000

    [skip actions] lab/lab07/ch07_t04_transportation.py

--- lab/lab07/ch07_t04_transportation.py
+++ lab/lab07/ch07_t04_transportation.py
@@ -1,7 +1,7 @@
 def hotel_cost(nights: int) -> int:
     return 140 * nights
 
-def plane_ride_cost(city: str) -> int:
+def plane_ride_cost(city:str) -> int:
     if city == "Charlotte":
         return 183
     elif city == "Tampa":
