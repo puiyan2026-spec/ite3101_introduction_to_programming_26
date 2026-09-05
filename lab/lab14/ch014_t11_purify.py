34764a90a30f6825a560a13735738940addd4744
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 03:32:37 +0000

    lab/lab14/ch014_t11_purify.py

---
+++ lab/lab14/ch014_t11_purify.py
@@ -0,0 +1,11 @@
+
+from typing import List
+
+
+def purify(seq:List[int])->List[int]:
+    result =[]
+    for item in seq:
+        if item in seq:
+            if item %2 ==0:
+                result.append(item)
+    return result
