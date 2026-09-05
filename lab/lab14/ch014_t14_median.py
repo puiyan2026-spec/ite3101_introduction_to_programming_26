75748f6296542a844dfafb92e7ea4be9d64bef10
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 03:41:40 +0000

    [skip actions] lab/lab14/ch014_t14_median.py

--- lab/lab14/ch014_t14_median.py
+++ lab/lab14/ch014_t14_median.py
@@ -9,3 +9,6 @@ def median(lst: List[int]) -> float:
         index_1 = len(sorted_lst) // 2
         m = (sorted_lst[index_1] + sorted_lst[index_2]) / 2.0
         return m
+    else:
+        index = len(sorted_lst) //2
+        return sorted_1st[index]
