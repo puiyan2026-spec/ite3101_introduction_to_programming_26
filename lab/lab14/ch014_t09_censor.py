e128d31e00c0624dd05c3bd32cee1fd8eedcf290
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 03:27:01 +0000

    [skip actions] lab/lab14/ch014_t09_censor.py

--- lab/lab14/ch014_t09_censor.py
+++ lab/lab14/ch014_t09_censor.py
@@ -2,4 +2,6 @@ def censor(text:str,word:str)->str:
     result=[]
     for s in text.split():
         if s == word:
-            
+            result.append("*" len(word)) 
+        else:
+            result.append(s)    
