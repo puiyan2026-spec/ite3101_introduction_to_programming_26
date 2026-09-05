8116ff9235d8a53e04fcab1bf21a4e824f20f983
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 03:43:05 +0000

    [skip actions] lab/lab14/ch014_t14_median.py

--- lab/lab14/ch014_t14_median.py
+++ lab/lab14/ch014_t14_median.py
@@ -4,6 +4,7 @@ from typing import List
 
 def median(lst: List[int]) -> float:
     sorted_lst = sorted(lst)
+    length = len(sorted_lst)
     if len(sorted_lst) % 2 == 0:
         index_1 = len(sorted_lst) // 2 - 1
         index_1 = len(sorted_lst) // 2
