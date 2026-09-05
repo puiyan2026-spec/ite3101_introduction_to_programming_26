515b59707bf47df6452eee653ecb6ad5d09ab404
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 03:33:24 +0000

    [skip actions] lab/lab14/ch014_t12_product.py

--- lab/lab14/ch014_t11_purify.py
+++ lab/lab14/ch014_t11_purify.py
@@ -2,10 +2,10 @@
 from typing import List
 
 
-def purify(seq:List[int])->List[int]:
-    result =[]
+def purify(seq: List[int]) -> List[int]:
+    result = []
     for item in seq:
         if item in seq:
-            if item %2 ==0:
+            if item % 2 == 0:
                 result.append(item)
     return result
