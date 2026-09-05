727dcf330dc1b01ad5020c7671f40a264e8e6c90
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 04:51:41 +0000

    [skip actions] lab/lab07/ch07_t06_never_know.py

--- lab/lab07/ch07_t06_never_know.py
+++ lab/lab07/ch07_t06_never_know.py
@@ -22,5 +22,5 @@ def rental_car_cost(days: int) -> int:
     return cost
 
 
-def trip_cost(city: str, days: int,) -> int:
+def trip_cost(city: str, days: int, spending_money: int = 0) -> int:
     return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city)
