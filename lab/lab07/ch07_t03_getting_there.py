b6d7b4ea8fcf7d2c238e302a4b26da305ba60590
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-05 03:58:45 +0000

    [skip actions] lab/lab07/ch07_t03_getting_there.py

--- lab/lab07/ch07_t03_getting_there.py
+++ lab/lab07/ch07_t03_getting_there.py
@@ -4,6 +4,6 @@ def hotel_cost(nights: int) -> int:
 def plane_ride_cost(city:str)->int:
     if city =="Charlotte":
         return 183
-    elif city == "Pittsburgh":
+    elif city == "Tampa":
         return 220 
     elif city == "Los Angles"
