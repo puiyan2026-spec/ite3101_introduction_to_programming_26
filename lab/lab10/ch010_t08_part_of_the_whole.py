fd221e01ff7900226235a8af6e965b22067f0d96
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-08 16:39:40 +0000

    lab/lab10/ch010_t08_part_of_the_whole.py

--- lab/lab10/ch010_t08_part_of_the_whole.py
+++ lab/lab10/ch010_t08_part_of_the_whole.py
@@ -48,7 +48,7 @@ def get_letter_grade(score: float) -> str:
 print(get_letter_grade(get_average(lloyd)))
 
 # Add your function below!
-def get_class_average(class_List: List[dict]) -> float:
+def get_class_average(class_list: List[dict]) -> float:
     student_averages: List[float] = []
     for student in class_list:
         student_avg = get_average(student)
