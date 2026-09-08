3604c5dfbdd5d61306266cea196482da8038b679
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-08 17:31:26 +0000

    [skip actions] lab/lab11/ch011_t16_str_in_list_in_func.py

--- lab/lab11/ch011_t16_str_in_list_in_func.py
+++ lab/lab11/ch011_t16_str_in_list_in_func.py
@@ -4,7 +4,7 @@ from typing import List
 
 n = ["Michael", "Lieberman"]
 # Add your function here
-def join_strings(words: List[str])
-    
+def join_strings(words: List[str])-str:
+ 
 
 # print(join_strings(n))
