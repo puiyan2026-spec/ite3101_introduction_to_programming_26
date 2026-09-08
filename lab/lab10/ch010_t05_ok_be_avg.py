49a4a1128bbbc1e260caafa0ef6060ed5ad87024
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-08 16:17:12 +0000

    [skip actions] lab/lab10/ch010_t05_ok_be_avg.py

--- lab/lab10/ch010_t05_ok_be_avg.py
+++ lab/lab10/ch010_t05_ok_be_avg.py
@@ -18,6 +18,6 @@ tyler = {
 }
 
 # Add your function below!
-def average(numbers: Lis[float]) -> float:
+def average(numbers: List[float]) -> float:
     total = sum(numbers)
     return total / len(numbers)
