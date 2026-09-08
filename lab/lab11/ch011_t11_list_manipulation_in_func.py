f7c8b5cfbf5b90fe7ecef22397f403d7c1bcdb55
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-08 17:12:43 +0000

    [skip actions] lab/lab11/ch011_t11_list_manipulation_in_func.py

--- lab/lab11/ch011_t11_list_manipulation_in_func.py
+++ lab/lab11/ch011_t11_list_manipulation_in_func.py
@@ -4,9 +4,10 @@ from typing import List
 n = [3, 5, 7]
 # Add your function here
 
-def list_extender(l:List[int])->List[int]:
+
+def list_extender(l: List[int]) -> List[int]:
     1.append(9)
     return l
 
-print(list_extender(n))
 
+print(list_extender(n))
