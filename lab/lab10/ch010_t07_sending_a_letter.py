069e027eab25d8c0bab34b9b0b4b38f453c94650
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-08 16:23:49 +0000

    [skip actions] lab/lab10/ch010_t07_sending_a_letter.py

--- lab/lab10/ch010_t07_sending_a_letter.py
+++ lab/lab10/ch010_t07_sending_a_letter.py
@@ -41,4 +41,6 @@ def get_average(student: dict) -> float:
     return 0.1 * homework + 0.3 * quizzes + 0.6 * tests
 
 # Add your function below!
-def get_letter_grade(score:float)
+def get_letter_grade(score:float)-> str:
+    if score>=90:
+        return "A"
