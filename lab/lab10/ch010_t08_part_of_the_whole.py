4798502c8640fa62f6b031b9796e25864a64adbc
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-08 16:33:35 +0000

    [skip actions] lab/lab10/ch010_t08_part_of_the_whole.py

--- lab/lab10/ch010_t08_part_of_the_whole.py
+++ lab/lab10/ch010_t08_part_of_the_whole.py
@@ -53,3 +53,4 @@ def get_class_average(class_List[dict])->float:
     for student in class_list:
         student_avg = get_average(student)
         student_averages.append(student_avg)
+    return average(student_averages)
