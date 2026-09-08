69ab237d76915fa7147b4fd63c9f7ea581659d15
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-08 17:34:24 +0000

    [skip actions] lab/lab11/ch011_t17_two_lists_two_argu_in_func.py

--- lab/lab11/ch011_t17_two_lists_two_argu_in_func.py
+++ lab/lab11/ch011_t17_two_lists_two_argu_in_func.py
@@ -6,4 +6,4 @@ def join_lists(x: List[int], y: List[int])-> List[int]:
     return x + y 
 
 print(join_lists(m, n))
- You want this to print [1, 2, 3, 4, 5, 6]
+You want this to print [1, 2, 3, 4, 5, 6]
