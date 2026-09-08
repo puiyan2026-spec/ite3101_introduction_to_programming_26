730f0cbe95f96538b540a0f882f5e46d7c0704b6
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-08 16:21:32 +0000

    [skip actions] lab/lab10/ch010_t06_just_weight_n_see.py

--- lab/lab10/ch010_t06_just_weight_n_see.py
+++ lab/lab10/ch010_t06_just_weight_n_see.py
@@ -30,5 +30,5 @@ def average(numbers: List[float]) -> float:
 def get_average(student: dict) -> float:
     homework = average(student["homework"])
     quizzes = average(student["quizzes"])
-    homework = average(student["homework"])
+    tests = average(student["homework"])
     homework = average(student["homework"])
