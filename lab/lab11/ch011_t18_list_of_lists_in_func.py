faae94aa44362e4eef57ddf8fd11f0b5b023276c
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-08 17:37:47 +0000

    [skip actions] lab/lab11/ch011_t18_list_of_lists_in_func.py

--- lab/lab11/ch011_t18_list_of_lists_in_func.py
+++ lab/lab11/ch011_t18_list_of_lists_in_func.py
@@ -7,5 +7,6 @@ n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
 def flatten(lists: List[List[int]])->List[int]:
     result
     for level1 in lists:
-        for level
+        for level2 in level1:
+            result
 # print(flatten(n))
