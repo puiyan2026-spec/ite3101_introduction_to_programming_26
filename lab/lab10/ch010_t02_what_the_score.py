453f97004c44eae2e8779f9f3b20816c25b7fdf3
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-08 16:08:43 +0000

    [skip actions] lab/lab10/ch010_t02_what_the_score.py

--- lab/lab10/ch010_t02_what_the_score.py
+++ lab/lab10/ch010_t02_what_the_score.py
@@ -1,8 +1,8 @@
 lloyd = {
     "name": "Lloyd",
-    "homework": [90.0, 97.0, 75.0, 92,0],
-    "quizzes": [88.0,40.0,],
-    "tests": []
+    "homework": [90.0, 97.0, 75.0, 92, 0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
 }
 alice = {
     "name": "Alice",
