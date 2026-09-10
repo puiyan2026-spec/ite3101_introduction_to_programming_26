8af8e2ef3c7aad769bf6d98f4e0a8f9c310c59ba
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-10 14:55:43 +0000

    lab/lab12/ch012_t10_danger_wr.py

--- lab/lab12/ch012_t10_danger_wr.py
+++ lab/lab12/ch012_t10_danger_wr.py
@@ -34,5 +34,5 @@ if guess_row == ship_row and guess_col == ship_col:
     print("Congratulations! You sank my battleship!")
 else:
     print("You missed my battleship!")
-    board[guess_row][guess_col] = "x"
+    board[guess_row][guess_col] = "X"
     print_board(board)
