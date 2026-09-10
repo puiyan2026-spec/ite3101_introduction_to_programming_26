c16d503dc42e846c2ec73080e6077a2e42418f52
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-10 15:53:40 +0000

    lab/lab13/ch013_t15_count_as_you_go.py

--- lab/lab13/ch013_t15_count_as_you_go.py
+++ lab/lab13/ch013_t15_count_as_you_go.py
@@ -1,5 +1,5 @@
 choices = ['pizza', 'pasta', 'salad', 'nachos']
 
-print('Your choices are:')
+print('Your choices are:')   
 for index, item in enumerate(choices):
-    print(index, item)
+    print(index + 1, item)
