55b68521926e17930cdd2a145ee301cb65c78ca7
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-10 15:27:36 +0000

    [skip actions] lab/lab12/ch012_t14_play_it_sam.py

--- lab/lab12/ch012_t14_play_it_sam.py
+++ lab/lab12/ch012_t14_play_it_sam.py
@@ -26,19 +26,17 @@ ship_col = random_col(board)
 print(ship_row)
 print(ship_col)
 
-for turn in range(4):
-    print("Turn", turn + 1)
-    # Write your code below!
-    guess_row = int(input("Guess Row: "))
-    guess_col = int(input("Guess Col: "))
-    if guess_row == ship_row and guess_col == ship_col:
-       print("Congratulations! You sank my battleship!")
+# Write your code below!
+guess_row = int(input("Guess Row: "))
+guess_col = int(input("Guess Col: "))
+if guess_row == ship_row and guess_col == ship_col:
+    print("Congratulations! You sank my battleship!")
+else:
+    if guess_row not in range(5) or guess_col not in range(5):
+        print("Oops, that's not even in the ocean.")
+    elif board[guess_row][guess_col] == "X":
+        print("You guessed that one already.")
     else:
-       if guess_row not in range(5) or guess_col not in range(5):
-            print("Oops, that's not even in the ocean.")
-        elif board[guess_row][guess_col] == "X":
-            print("You guessed that one already.")
-         else:
-            print("You missed my battleship!")
-            board[guess_row][guess_col] = "X":
-            print_board(board)
+        print("You missed my battleship!")
+        board[guess_row][guess_col] = "X"
+    print_board(board)
