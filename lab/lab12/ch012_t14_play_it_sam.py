5a2b570e2bd4547935fb6e9bd03d5573a69de02c
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-10 15:22:30 +0000

    [skip actions] lab/lab12/ch012_t14_play_it_sam.py

--- lab/lab12/ch012_t14_play_it_sam.py
+++ lab/lab12/ch012_t14_play_it_sam.py
@@ -27,7 +27,7 @@ print(ship_row)
 print(ship_col)
 
 for turn in range(4):
-    print("Turn", )
+    print("Turn", turn + 1)
 # Write your code below!
 guess_row = int(input("Guess Row: "))
 guess_col = int(input("Guess Col: "))
