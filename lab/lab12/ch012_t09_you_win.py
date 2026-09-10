55ce9261dd7a52b9ad8684f74dfdacb7c757ef8f
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-10 14:44:07 +0000

    lab/lab12/ch012_t09_you_win.py

--- lab/lab12/ch012_t09_you_win.py
+++ lab/lab12/ch012_t09_you_win.py
@@ -31,7 +31,7 @@ guess_col = int(input("Guess Col: "))
 
 # Write your code below!
 if guess_row == ship_row and guess_col == ship_col:
-    print("Congratulations! You sank mu battleship!")
+    print("Congratulations! You sank my battleship!")
 else:
     print("You missed my battleship!")
     board[guess_row][guess_col] = "x"
