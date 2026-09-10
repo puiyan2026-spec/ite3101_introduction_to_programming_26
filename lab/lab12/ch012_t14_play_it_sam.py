2b9df4362b9ff4efbd7e4758858884ba101536b5
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-10 15:23:12 +0000

    [skip actions] lab/lab12/ch012_t14_play_it_sam.py

--- lab/lab12/ch012_t14_play_it_sam.py
+++ lab/lab12/ch012_t14_play_it_sam.py
@@ -35,10 +35,10 @@ for turn in range(4):
        print("Congratulations! You sank my battleship!")
     else:
        if guess_row not in range(5) or guess_col not in range(5):
-        print("Oops, that's not even in the ocean.")
+            print("Oops, that's not even in the ocean.")
         elif board[guess_row][guess_col] == "X":
-        print("You guessed that one already.")
+            print("You guessed that one already.")
         else:
-        print("You missed my battleship!")
-        board[guess_row][guess_col] = "X"
-    print_board(board)
+            print("You missed my battleship!")
+            board[guess_row][guess_col] = "X"
+            print_board(board)
