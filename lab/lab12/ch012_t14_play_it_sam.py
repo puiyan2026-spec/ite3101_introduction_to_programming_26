382fd29be29b9572af9a0de04ec23b58e6fdd5ae
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-10 15:26:25 +0000

    [skip actions] lab/lab12/ch012_t14_play_it_sam.py

--- lab/lab12/ch012_t14_play_it_sam.py
+++ lab/lab12/ch012_t14_play_it_sam.py
@@ -40,5 +40,5 @@ for turn in range(4):
             print("You guessed that one already.")
          else:
             print("You missed my battleship!")
-            board[guess_row][guess_col] = "X"
+            board[guess_row][guess_col] = "X":
             print_board(board)
