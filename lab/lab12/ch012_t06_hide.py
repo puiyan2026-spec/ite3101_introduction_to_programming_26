fbc5bb50cb72625ee577772bdb27d9b53862c8cc
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-10 08:51:41 +0000

    [skip actions] lab/lab12/ch012_t06_hide.py

--- lab/lab12/ch012_t06_hide.py
+++ lab/lab12/ch012_t06_hide.py
@@ -13,4 +13,4 @@ def print_board(board_in: List[List[str]]) -> None:
 
 # Add your code below!
 def random_row(board_in:List[List[str]])->int:
-    returm randint(0, len(board_in))
+    returm randint(0, len(board_in)-1)
