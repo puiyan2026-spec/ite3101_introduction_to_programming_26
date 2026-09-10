cf5726f3efc53b3dc22748a170f8c9472a3e6d2e
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-10 15:13:24 +0000

    [skip actions] lab/lab12/ch012_t11_bad_aim.py

--- lab/lab12/ch012_t11_bad_aim.py
+++ lab/lab12/ch012_t11_bad_aim.py
@@ -31,7 +31,7 @@ guess_col = int(input("Guess Col: "))
 
 # Write your code below!
 if guess_row == ship_row and guess_col == ship_col:
-    print("Congratulations! You sank my battleship!" )
+    print("Congratulations! You sank my battleship!")
 else:
     if guess_row not in range(5) or guess_col not in range(5):
         print("Oops, that's not even in the ocrean.")
