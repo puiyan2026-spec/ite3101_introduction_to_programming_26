36f31e8e8536585923b140ca4bdb64b446d4944b
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-10 14:59:30 +0000

    [skip actions] lab/lab12/ch012_t11_bad_aim.py

--- lab/lab12/ch012_t11_bad_aim.py
+++ lab/lab12/ch012_t11_bad_aim.py
@@ -34,7 +34,7 @@ if guess_row == ship_row and guess_col == ship_col:
     print("Congratulations! You sank my battleship!")
 else:
     if guess_row not in range(5) or guess_col not in range(5)
-         print("Oops, that's not even in the ocrean.")
+    print("Oops, that's not even in the ocrean.")
     else:
     print("You missed my battleship!")
     board[guess_row][guess_col] = "X"
