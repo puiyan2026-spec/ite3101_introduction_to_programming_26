550ca23b0b0b9a9644278e432cd7d0e572cdb814
puiyan2026-spec <puiyan2026@gmail.com>
2026-09-10 15:39:14 +0000

    [skip actions] lab/lab13/ch013_t01_while_you_are_here.py

--- lab/lab13/ch013_t01_while_you_are_here.py
+++ lab/lab13/ch013_t01_while_you_are_here.py
@@ -3,6 +3,6 @@ count = 0
 if count < 5:
     print("Hello, I am an if statement and count is", count)
 
-while count < 5:
+while count < 9:
     print("Hello, I am a while and count is", count)
     count += 1
